c02c215a33dba5cd6de472ecbec01a007a2e7d91
izumix03 <takahiro.izumikawa@gmail.com>
2023-06-23 15:52:53 +0900

    finish

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use crate::helpers::DynError;
 mod engine;
 mod helpers;
 
+// cargo run abc* regex.tex
 fn main() -> Result<(), DynError> {
     let args: Vec<String> = env::args().collect();
     if args.len() <= 2 {
@@ -39,21 +40,46 @@ fn match_file(expr: &str, file: &str)-> Result<(), DynError> {
 
 #[cfg(test)]
 mod tests {
-    use std::mem::take;
+    use crate::engine::do_matching;
+    use crate::helpers::{safe_add, SafeAdd};
 
     #[test]
-    fn take_sample() {
-        let mut n = Some(10);
-        let v = take(&mut n);
-        println!("n = {:?}, v = {:?}", n, v);
+    fn test_safe_add() {
+        let u = 10;
+        assert_eq!(Some(30), u.safe_add(&20));
+
+        let u = !0; // 2^64 -1 (64bit cpuの場合) 18446744073709551615
+        assert_eq!(None, u.safe_add(&1));
+
+        let u = 18446744073709551614; // ギリギリ足せる
+        assert_eq!(Some(18446744073709551615), u.safe_add(&1));
+
+        // エラーのクロージャ渡してもOK
+        let mut n = 10;
+        assert!(safe_add(&mut n, &20, || ()).is_ok());
+
+        // エラーの確認
+        let mut n = !0;
+        assert!(safe_add(&mut n, &1, || ()).is_err());
     }
 
     #[test]
-    fn unicode() {
-        let s = "aはAの小文字".as_bytes();
-        for i in s {
-            print!("{:x}", i);
-        }
-        println!();
+    fn test_matching() {
+        // パースエラー
+        assert!(do_matching("+b", "bbb", true).is_err());
+        assert!(do_matching("*b", "bbb", true).is_err());
+        assert!(do_matching("|b", "bbb", true).is_err());
+        assert!(do_matching("?b", "bbb", true).is_err());
+
+        // マッチ成功
+        assert!(do_matching("abc|def", "def", true).unwrap());
+        assert!(do_matching("(abc)*", "abcabc", true).unwrap());
+        assert!(do_matching("(ab|cd)+", "abcdcd", true).unwrap());
+        assert!(do_matching("abc?", "ab", true).unwrap());
+
+        // マッチしない
+        assert!(!do_matching("abc|def", "efa", true).unwrap());
+        assert!(!do_matching("(ab|cd)+", "", true).unwrap());
+        assert!(!do_matching("abc?", "acb", true).unwrap());
     }
 }
